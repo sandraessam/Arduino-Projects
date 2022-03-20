c71488966389a0af50a086672fa41655e9cdc22d
sandraessam <53277522+sandraessam@users.noreply.github.com>
2022-03-20 18:53:02 +0200

    Add files via upload

---
+++ car_controlled_with_gesture/reciever_bluetooth.ino
@@ -0,0 +1,103 @@
+/*this is slave
+ هذا الكود للreciever
+ */
+
+#include <SoftwareSerial.h>
+#include<Wire.h>
+SoftwareSerial BTSerial(10, 11); //TX//RX
+const int ledPin = 3;
+//Motor A
+const int motorPin1  = 5;
+const int motorPin2  = 6 ; 
+//Motor B
+const int motorPin3  = 8;
+const int motorPin4  = 7;
+int state = 0;
+
+void setup()
+{
+  BTSerial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+    Serial.begin(9600); // Default communication rate of the Bluetooth module
+}
+void loop()
+{
+ if(BTSerial.available() > 0)
+ { 
+    // Checks whether data is comming from the serial port
+    state = BTSerial.read(); // Reads the data from the serial port
+ }
+ 
+       if (state == 's') //Stationary
+      {
+
+        digitalWrite(ledPin, LOW);
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin2, LOW);//STOP
+        digitalWrite(motorPin3, LOW);
+        digitalWrite(motorPin4, LOW);
+        Serial.print('s');
+        state = 0;
+
+      
+
+      }
+      else
+      {
+
+         if (state == 'f') //Forward
+        {
+          digitalWrite(ledPin, HIGH);
+          digitalWrite(motorPin1, LOW);
+          digitalWrite(motorPin2, HIGH);//FORWARD
+          digitalWrite(motorPin3, HIGH);
+          digitalWrite(motorPin4, LOW);
+          Serial.print('f');
+          state = 0;
+        }
+
+        if (state == 'b')//Backward
+        {
+          digitalWrite(ledPin, LOW);
+          digitalWrite(motorPin1, HIGH);
+          digitalWrite(motorPin2, LOW);//REVERSE
+          digitalWrite(motorPin3, LOW);
+          digitalWrite(motorPin4, HIGH);
+          Serial.print('b');
+          state = 0;
+
+
+        }
+
+          if (state == 'l') //Left
+        {
+          digitalWrite(ledPin, HIGH);
+          digitalWrite(motorPin1, HIGH);
+          digitalWrite(motorPin2, LOW);//LEFT
+          digitalWrite(motorPin3, HIGH);
+          digitalWrite(motorPin4, LOW);
+          Serial.print('l');
+          state = 0;
+
+        }
+
+         if (state == 'r') //Right
+        {
+          digitalWrite(ledPin, HIGH);
+          digitalWrite(motorPin1, LOW);
+          digitalWrite(motorPin2, HIGH);//RIGHT
+          digitalWrite(motorPin3, LOW);
+          digitalWrite(motorPin4, HIGH);
+          Serial.print('r');
+          state = 0;
+        }
+      }
+
+      delay(333);
+    }
+  
+
---
+++ car_controlled_with_gesture/transmitter_bluetooth.ino
@@ -0,0 +1,90 @@
+/*this is Master
+ هذا الكود للtransmitter
+ */
+
+#include <SoftwareSerial.h>
+#include<Wire.h>
+
+SoftwareSerial BTSerial(10, 11); //TX//RX
+
+int state = 0;
+const int MPU_addr = 0x68; // I2C address of the MPU-6050
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+const int trigpin =13;
+
+
+void setup() 
+{
+  BTSerial.begin(9600);
+  pinMode(trigpin, OUTPUT);
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
+  }
+
+  void loop() {
+
+    
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true);
+  AcX = Wire.read() << 8 | Wire.read();
+  AcY = Wire.read() << 8 | Wire.read();
+  AcZ = Wire.read() << 8 | Wire.read();
+  Tmp = Wire.read() << 8 | Wire.read();
+  GyX = Wire.read() << 8 | Wire.read();
+  GyY = Wire.read() << 8 | Wire.read();
+  GyZ = Wire.read() << 8 | Wire.read();
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp / 340.00 + 36.53);
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+ 
+
+ 
+ if (AcX < -6000 && -11000)
+  {
+  
+ BTSerial.write('b');
+ digitalWrite(trigpin,HIGH);
+      
+  }
+  else if (AcX > 4000 && 6000)
+  {
+     
+   BTSerial.write('f');
+  digitalWrite(trigpin,LOW );
+  }
+  else if (AcY < -7000 && -5000)
+  {
+   
+   BTSerial.write('r');
+
+ digitalWrite(trigpin,HIGH);
+  }
+  else if (AcY > 5000 && 12000)
+  {
+     
+  BTSerial.write('l');
+ digitalWrite(trigpin,HIGH);
+  }
+  else
+  {
+     BTSerial.write('s');
+   
+    digitalWrite(trigpin,LOW);
+  
+   
+  
+
+  }
+  delay(333);
+}
+ 
